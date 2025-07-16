c6db8c03b82047e6b85736b7bfa4689d860d3032
DiogoAlip <diogoalipazaga@gmail.com>
2025-07-16 06:37:14 -0500

    feat: medical view added

--- routes/perfildoctor.routes.js
+++ routes/perfildoctor.routes.js
@@ -4,13 +4,17 @@ const { pacientes } = require("../data/pacientes");
 const { getColorByStatus } = require("../helpers/getColorByStatus");
 
 router.get("/perfildedoctor", (req, res) => {
-  res.render("perfildedoctor", { pacientes });
+  res.render("perfildedoctor", { pacientes, getColorByStatus });
 });
 
 router.get("/paciente/:id_paciente", (req, res) => {
   const paciente = pacientes.find((p) => p.id === req.params.id_paciente);
   if (!paciente) return res.status(404).send("Paciente no encontrado");
   res.render("paciente", { ...paciente, getColorByStatus });
+});
+
+router.get("/diagnostico", (req, res) => {
+  res.render("diagnostico");
 });
 
 module.exports = router;
